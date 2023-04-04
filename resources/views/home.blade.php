2f517607287d2450ed504e0bbfc27ae9ecdd17d6
ABiviano13 <abiviano0@outlook.com>
2023-04-04 15:52:33 +0200

    Create file php + text

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primi Passi Laravel</title>
+</head>
+<body>
+
+    <main>
+
+        <section>
+            <h1>Hello World!</h1>
+            <p>
+                Questi sono i miei primi passi nel meraviglioso mondo di Laravel!
+            </p>
+        </section>
+
+    </main>
+    
+</body>
+</html>
