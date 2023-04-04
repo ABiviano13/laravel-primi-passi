2ae02168c36508b723ee845833cee7c0cf0eae06
ABiviano13 <abiviano0@outlook.com>
2023-04-04 15:57:49 +0200

    BONUS  FEAT contacts

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contatti</title>
+</head>
+<body>
+
+    <main>
+        <section>
+            <h1>
+                Contatti che potrebbero servirti:
+            </h1>
+            <p>
+                Email: ...@...com;
+                Numero di telefono: 333 1902 ...;
+            </p>
+
+        </section>
+    </main>
+    
+</body>
+</html>
